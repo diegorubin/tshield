6bfb568c42e0c5f1c29adbc706552c57c41ea83a
Diego Rubin <rubin.diego@gmail.com>
2016-10-29 17:14:02 -0200

    configuration class.

--- lib/tshield/configuration.rb
+++ lib/tshield/configuration.rb
@@ -3,12 +3,29 @@ module TShield
 
     attr_accessor :domains
 
-    def singleton
+    def initialize(attributes)
+      attributes.each do |key, value|
+        send("#{key}=", value)
+      end
+    end
+
+    def self.singleton
       @@configuration ||= load_configuration
     end
 
+    def get_domain_for(path)
+      domains.each do |url, paths|
+        return url if paths.include?(path)
+      end
+      nil
+    end
+
     private
-    def load_configuration
+    def self.load_configuration
+      config_path = File.join('config', 'tshield.yml')
+      file = File.open(config_path)
+      configs = YAML::load(file.read)
+      Configuration.new(configs)
     end
 
   end
--- spec/tshield/configuration_spec.rb
+++ spec/tshield/configuration_spec.rb
@@ -1,9 +1,32 @@
 require 'spec_helper'
 
 describe TShield::Configuration do
-  describe 'load configurations' do
-    it 'recover configs from yaml' do
+  before :each do 
+    allow(File).to(
+      receive(:join).and_return('spec/tshield/fixtures/config/tshield.yml'))
+    @configuration = TShield::Configuration.singleton
+  end
+
+  describe 'load configurations from yaml' do
+    it 'recover domains' do
+      expect(@configuration.domains['example.org']).to(
+        include('/api/one', '/api/two'))
     end
   end
+
+  describe 'get_domain_for' do
+    it 'return domain for example.org' do
+      expect(@configuration.get_domain_for('/api/two')).to eq('example.org')
+    end
+
+    it 'return domain for example.com' do
+      expect(@configuration.get_domain_for('/api/three')).to eq('example.com')
+    end
+
+    it 'return nil if domain not found' do
+      expect(@configuration.get_domain_for('/api/four')).to be_nil
+    end
+  end
+
 end
 
