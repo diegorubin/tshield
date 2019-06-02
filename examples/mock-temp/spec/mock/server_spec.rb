7ec975e6ef51b1e856d63237b6a110ccdebb65ea
eduardongarcia <dudueco@hotmail.com>
2019-06-02 16:25:53 -0300

    Fix mistake to create example

---
+++ examples/mock-temp/spec/mock/server_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Mock::Server do
+  it "has a version number" do
+    expect(Mock::Server::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
