ca6f16c1b2e94e21d4562d98a1963ec28cd4e951
eduardongarcia <dudueco@hotmail.com>
2019-06-09 17:06:11 -0300

    remove private keys

--- examples/client-api-nodejs/client-api/config.js
+++ examples/client-api-nodejs/client-api/config.js
@@ -1,11 +1,11 @@
 const MARVEL_API = process.env.MARVEL_API || 'https://gateway.marvel.com';
 const MARVEL_API_CHARACTERS = process.env.MARVEL_API_CHARACTERS || '/v1/public/characters';
-const MARVEL_API_PRIVATE_KEY = process.env.MARVEL_API_PRIVATE_KEY || 'ca2d7a653a41e39631509acfe733c0b4490ff6bd';
-const MARVEL_API_PUBLIC_KEY = process.env.MARVEL_API_PUBLIC_KEY || '13fd445760a05bf47e824f6ea98c8044';
+const MARVEL_API_PRIVATE_KEY = process.env.MARVEL_API_PRIVATE_KEY || 'mock-marvel-private-key';
+const MARVEL_API_PUBLIC_KEY = process.env.MARVEL_API_PUBLIC_KEY || 'mock-marvel-public-key';
 
 const TENOR_API = process.env.TENOR_API || 'https://api.tenor.com';
 const TENOR_API_SEARCH = process.env.TENOR_API_SEARCH || '/v1/search';
-const TENOR_API_KEY = process.env.TENOR_API_KEY || 'WR5S9AXCE52N';
+const TENOR_API_KEY = process.env.TENOR_API_KEY || 'mock-tenor-key';
 
 const applicationConfig = {
   marvel: {
