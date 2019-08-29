6b6e1ba6d923f098024623da9cd9b46c56d5c3cc
Diego Rubin <rubin.diego@gmail.com>
2019-08-29 16:27:39 -0300

    fix #30

--- lib/tshield/controllers/sessions.rb
+++ lib/tshield/controllers/sessions.rb
@@ -11,27 +11,26 @@ module TShield
     module Sessions
       def self.registered(app)
         session_path = TShield::Configuration.singleton.session_path
-        ip = request.ip
-        register_get(app, session_path, ip)
-        register_post(app, session_path, ip, params)
-        register_delete(app, session_path, ip)
+        register_get(app, session_path)
+        register_post(app, session_path)
+        register_delete(app, session_path)
       end
 
-      def self.register_get(app, session_path, ip)
+      def self.register_get(app, session_path)
         app.get session_path do
-          TShield::Sessions.current(ip).to_json
+          TShield::Sessions.current(request.ip).to_json
         end
       end
 
-      def self.register_post(app, session_path, params, ip)
+      def self.register_post(app, session_path)
         app.post session_path do
-          TShield::Sessions.start(ip, params[:name]).to_json
+          TShield::Sessions.start(request.ip, params[:name]).to_json
         end
       end
 
-      def self.register_delete(app, session_path, ip)
+      def self.register_delete(app, session_path)
         app.delete session_path do
-          TShield::Sessions.stop(ip).to_json
+          TShield::Sessions.stop(request.ip).to_json
         end
       end
     end
