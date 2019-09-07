008d37b974f6c58c4b6cd126b92766fed6597285
eduardongarcia <--help>
2019-09-07 12:34:40 -0300

    Clean mock-server example project

--- examples/client-api-nodejs/mock-server/lib/mock/server/version.rb
+++
@@ -1,5 +0,0 @@
-module Mock
-  module Server
-    VERSION = "0.1.0"
-  end
-end
--- examples/client-api-nodejs/mock-server/spec/mock/server_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Mock::Server do
-  it "has a version number" do
-    expect(Mock::Server::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
