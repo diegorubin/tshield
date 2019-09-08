72a5ee367be3ae56404857d4aa4528f7dccc20e7
Eduardo <dudueco@hotmail.com>
2019-09-08 18:12:15 -0300

    Implement features

--- examples/component-test/app/tests/components/features/step_definitions/home_steps.rb
+++ examples/component-test/app/tests/components/features/step_definitions/home_steps.rb
@@ -1,11 +1,27 @@
 # frozen_string_literal: true
 
-Quando('acesso a página e realizo uma busca válida') do
-  start_session('valid')
+Given("a {string} session") do |session|
+  start_session(session)
+end
+
+When("visit home page") do
   @page = HomePage.new
   @page.load
 end
 
-Então('vejo o resultado da busca') do
-  expect(page).to have_current_path('/')
+And("do search with {string}") do |search|
+  @page.do_search(search)
+end
+
+
+Then("show result") do |table|
+  expect(@page.card_result_name).to have_text(table.hashes[0][:name])
+  expect(@page.card_result_comic).to have_text(table.hashes[0][:first_comic])
+  expect(@page.card_result_img[:src]).to have_text(table.hashes[0][:gif_url])
+end
+
+
+Then("show error") do |table|
+  expect(@page.card_error_title).to have_text(table.hashes[0][:title])
+  expect(@page.card_error_msg).to have_text(table.hashes[0][:message])
 end
