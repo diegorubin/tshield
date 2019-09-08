d72f54a444981499444559973f8b40001f36f8da
Eduardo <dudueco@hotmail.com>
2019-09-08 17:16:55 -0300

    Make home page

--- examples/component-test/app/src/gateways/http/router.js
+++ examples/component-test/app/src/gateways/http/router.js
@@ -1,38 +1,17 @@
 const bodyParser = require('body-parser');
-const marvelController = require('./marvelController');
+
+const searchController = require('./searchController');
 
 module.exports = (router) => {
+  router.use(bodyParser.urlencoded({ extended: false }));
   router.use(bodyParser.json());
 
-  /**
-   * @swagger
-   * /marvel-gif:
-   *  get:
-   *    description: return a marvel character full name and first comic with a tenor gif
-   *    produces:
-   *      - application/json
-   *    parameters:
-   *      -
-   *        name: "name"
-   *        in: "query"
-   *        description: "name to filter by"
-   *        required: true
-   *        type: "string"
-   *    responses:
-   *      '200':
-   *        description: |-
-   *          200 response
-   *        examples:
-   *          application/json: |-
-   *            {
-   *              "name": "Spider-Man",
-   *              "firstComic": "Spider-Man: 101 Ways to End the Clone Saga (1997) #1",
-   *              "gifUrl": "https://media.tenor.com/images/837072ca19e3c5cebea76e2693f3100d/tenor.gif",
-   *            }
-   *
-   */
-  router.get('/marvel-gif', (req, res) => {
-    marvelController.marvelGif(req, res);
+  router.get('/', (req, res) => {
+    res.render('home');
+  });
+
+  router.post('/', (req, res) => {
+    searchController.search(req, res);
   });
 
   return router;
